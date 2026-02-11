eaf07ea716b0ae449b64df500f2cdee6b86eaf8d
ItayBe <itaybe777@gmail.com>
2026-02-11 14:59:15 +0200

    feat: add PATCH to update the car status

--- src/api/router/car.py
+++ src/api/router/car.py
@@ -11,23 +11,34 @@ class CarCreate(BaseModel):
     year: int
 
 
+class CarStatusUpdate(BaseModel):
+    status: CarStatus
+
+
 car_router = APIRouter(prefix="/car", tags=["Car"])
 
 
 @car_router.post("/")
 def create_car(car: CarCreate, db: Session = Depends(get_db)):
     service = CarService(db)
-    return service.add_car(model=car.model, year=car.year)
+    return service.add_car(car.model, car.year)
 
 
 @car_router.get("/")
 def list_all_cars(status: str = None, limit: int = Query(default=5, ge=1, le=100), db: Session = Depends(get_db)):
     service = CarService(db)
     car_status = CarStatus(status) if status else None
-    return service.list_cars(status=car_status, limit=limit)
+    return service.list_cars(car_status, limit)
 
 
 @car_router.get("/{car_id}")
 def get_car_by_id(car_id: int, db: Session = Depends(get_db)):
     service = CarService(db)
     return service.get_car_by_id(car_id)
+
+
+@car_router.patch("/{car_id}")
+def update_car_status(car_id: int, new_status_payload: CarStatusUpdate, db: Session = Depends(get_db)):
+    service = CarService(db)
+    new_status = new_status_payload.status
+    return service.update_car_status_by_car_id(car_id, new_status)
