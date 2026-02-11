8923609977bf48a73b8aba8f82b3fe8f13dcdc05
ItayBe <itaybe777@gmail.com>
2026-02-12 00:29:37 +0200

    feat: mount prometheus to the lifespan of the app and move the creation of tables as well

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.api.router.car import car_router
 from src.api.router.rental import rental_router
 from src.db.session import create_tables
+from src.metrics import mount_metrics, update_gauges
 
-create_tables()
 
-app = FastAPI(title="DriveNow")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """ lifespan events """
+    create_tables()
+    update_gauges()
+    yield
+
+
+app = FastAPI(title="DriveNow", lifespan=lifespan)
 app.include_router(car_router)
 app.include_router(rental_router)
+mount_metrics(app)
