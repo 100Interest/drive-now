c6090b75534f2c91a6dda180e5a5bc32120a5be9
ItayBe <itaybe777@gmail.com>
2026-02-11 15:06:58 +0200

    feat: remove unnecessary car code in rental router and add end date payload to the end_rental endpoint

--- src/api/router/rental.py
+++ src/api/router/rental.py
@@ -1,18 +1,15 @@
+import datetime
+from typing import Optional
+
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 
-from src.db.models import CarStatus
 from src.db.session import Session, get_db
 from src.services.rental import RentalService
 
 
-class CarCreate(BaseModel):
-    model: str
-    year: int
-
-
-class CarStatusUpdate(BaseModel):
-    status: CarStatus
+class EndRentalPayload(BaseModel):
+    end_date: datetime.datetime = None  # None = use now
 
 
 rental_router = APIRouter(prefix="/rental", tags=["Rental"])
@@ -25,10 +22,11 @@ def create_rental(car_id: int, customer_name: str, db: Session = Depends(get_db)
 
 
 @rental_router.post("/{rental_id}/end")
-def end_rental(rental_id: int, db: Session = Depends(get_db)):
+def end_rental(rental_id: int, end_rental_payload: EndRentalPayload = None, db: Session = Depends(get_db)):
     service = RentalService(db)
+    end_date = end_rental_payload.end_date if end_rental_payload.end_date else None
     try:
-        rental = service.end_rental(rental_id)
+        rental = service.end_rental(rental_id, end_date)
         return rental
     except ValueError as err:
         raise HTTPException(status_code=404, detail=f"Rental id {rental_id} was not found\n{err}")
